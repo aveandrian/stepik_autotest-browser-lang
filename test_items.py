16b08eed96ffddf8f967c72fe8c18de75f4db2bd
aveandrian <trancemilly7@gmail.com>
2019-09-10 15:42:23 +0500

    Removed waiting (time.sleep(5) in script

--- test_items.py
+++ test_items.py
@@ -6,6 +6,6 @@ import time
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_guest_should_see_login_link(browser):
+def test_add_to_basket_button_present(browser):
     browser.get(link)
     assert EC.presence_of_element_located((By.CLASS_NAME, "btn-add-to-basket")), "Button not found"
